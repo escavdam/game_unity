24a8d3aa35637b701e1bea5616b1206153e6095e
camachoo18 <estivencamachops@gmail.com>
2024-02-07 11:44:37 +0100

    #añado gameevents

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -6,6 +6,14 @@ using UnityEngine.Events;
 public class GameEvents
 {
     public class MemeEvent : UnityEvent<Meme> { };
+    public class EnemyEvent: UnityEvent<StatsEnemy> { };
 
     public static MemeEvent MemeChosen = new MemeEvent();
+   /* public static EnemyEvent EnemyStats = new EnemyEvent();*/
+
+    public static EnemyEvent HurtPlayer = new EnemyEvent();
+    public static EnemyEvent HurtEnemy = new EnemyEvent();
+
+
+
 }
