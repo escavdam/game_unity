698e241d43313d75375105f1c6dcd50f8fe5a6bc
Alvaro <17alma41@gmail.com>
2024-02-07 12:06:47 +0100

    add-gameEvents

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -6,6 +6,14 @@ using UnityEngine.Events;
 public class GameEvents
 {
     public class MemeEvent : UnityEvent<Meme> { };
+    public class EnemyEvent : UnityEvent<StatsEnemy> { };
 
     public static MemeEvent MemeChosen = new MemeEvent();
+    /* public static EnemyEvent EnemyStats = new EnemyEvent();*/
+
+    public static UnityEvent HurtPlayer = new UnityEvent();
+    public static UnityEvent HurtEnemy = new UnityEvent();
+
+
+
 }
