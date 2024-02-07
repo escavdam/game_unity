a512d95b2c1d4d8416c2a55908a6e86fa835e451
Alvaro <17alma41@gmail.com>
2024-02-07 12:21:00 +0100

    fix-gameEvents

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -6,19 +6,6 @@ using UnityEngine.Events;
 public class GameEvents
 {
     public class MemeEvent : UnityEvent<Meme> { };
-<<<<<<< HEAD
-    public class EnemyEvent: UnityEvent<StatsEnemy> { };
-
-    public static MemeEvent MemeChosen = new MemeEvent();
-   /* public static EnemyEvent EnemyStats = new EnemyEvent();*/
-
-    public static EnemyEvent HurtPlayer = new EnemyEvent();
-    public static EnemyEvent HurtEnemy = new EnemyEvent();
-
-
-
-}
-=======
     public class EnemyEvent : UnityEvent<StatsEnemy> { };
 
     public static MemeEvent MemeChosen = new MemeEvent();
@@ -30,4 +17,3 @@ public class GameEvents
 
 
 }
->>>>>>> alvaro
